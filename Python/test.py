b25c4020ce4e9d855bec66b178f905d379609a02
supaseeka <thesil2007@gmail.com>
2025-10-09 16:21:41 +0100

    Updating files

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,9 @@
-print("Hello, world!")
+from scipy.optimize import fsolve 
+
+def find_nb(m):
+    for i in range(0, 100):
+        solution = fsolve(lambda x: i*x**3 - 3*((1/2)*i(i+1))*x**2 + 3*((1/6)*i*(i+1)*(2*i+1))*x -(1/4)*i**2*(i+1)**2)[0]
+        if type(solution) == int:
+            return solution
+        else:
+            return -1
